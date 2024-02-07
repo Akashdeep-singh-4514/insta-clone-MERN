7957fe49153796dac32c03f7e3c6274e047fda76
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2024-02-07 14:21:23 +0530

    npm i mongoose and connected with mongodb

--- Server/app.js
+++ Server/app.js
@@ -1,17 +1,22 @@
 const express = require("express")
-// require is used to import modules
 const app = express()
-PORT = 5000
-// fixing cross origin
-const cors = require("cors")
-app.use(cors("http://localhost:5173/signin"))
+port = 5000
+const mongoose = require("mongoose")
+const mongoUrl = require("./keys.js")
 
-const data = require("./data.js")
+require("./models/model.js")
+app.use(require("./routes/auth.js"))
 
-app.get("/", (req, res) => {
-    res.json(data)
+
+mongoose.connect(mongoUrl)
+
+mongoose.connection.on("connected", () => {
+    console.log("successfully connected");
+})
+mongoose.connection.on("error", () => {
+    console.log("not connected to mongodb");
 })
 
-app.listen(PORT, () => {
-    console.log("server at " + PORT);
+app.listen(port, () => {
+    console.log("server at " + port);
 })
--- Server/data.js
+++
@@ -1,7 +0,0 @@
-const data = [
-    { name: "user1", email: "email1@gmail.com" },
-    { name: "user2", email: "email2@gmail.com" },
-    { name: "user3", email: "email3@gmail.com" }
-]
-// exporting data
-module.exports = data
