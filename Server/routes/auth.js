b707492c00b92db49928f75da81c84bcfa1ff7f7
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2024-02-09 15:11:01 +0530

    updated signup

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -39,7 +39,7 @@ router.post("/signin", (req, res) => {
         bcrypt.compare(password, savedUser.password).then((match) => {
             if (match) {
                 const token = jwt.sign({ _id: savedUser._id }, jwt_secret)
-                return res.status(200).json({ loggedin: true, userData: savedUser, message: "signed in successfully", token: token })
+                return res.status(200).json({ userData: savedUser, message: "signed in successfully", token: token })
             }
             else {
                 return res.status(422).json({ error: "Invalid password" })
@@ -63,8 +63,18 @@ router.post("/signup", (req, res) => {
             const user = new USER({
                 name, userName, email, password: hashedPassword
             })
+
             user.save()
-                .then(user => { res.json({ loogedin: true, userData: user, message: "registered successfully" }) })
+                .then(user => {
+                    USER.findOne({ email: email }).then((savedUser) => {
+                        if (!savedUser) {
+                            return res.status(422).json({ error: "Error occured" })
+                        }
+                        const token = jwt.sign({ _id: savedUser._id }, jwt_secret)
+                        return res.status(200).json({ userData: savedUser, message: "signip in successfully", token: token })
+                    })
+
+                })
                 .catch(err => { console.log(err); })
         })
 
